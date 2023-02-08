f648aaf1033f30b53734002740998c5c9c7fee89
Emi <89019222+ENalb@users.noreply.github.com>
2023-02-08 09:28:07 +0400

    Update spreadsheet.cpp

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -8,12 +8,12 @@ Spreadsheet::Spreadsheet(int r, int c)
 	column = c;
 	Cell cell("1", Color::blue);
 	for (int i = 0; i < r; ++i) {
-    std::vector<Cell> row;
-    for (int j = 0; j < c; ++j) {
-      row.push_back(cell);
-    }
-    m_cells.push_back(row);
-  }
+		std::vector<Cell> row;
+		for (int j = 0; j < c; ++j) {
+			row.push_back(cell);
+		}
+		m_cells.push_back(row);
+  	}
 }
 
 int Spreadsheet::getRow()
@@ -53,41 +53,41 @@ void Spreadsheet::removeRow(int r)
 void Spreadsheet::addColumn(int c, const std::vector<Cell>& column)
 {
 	int rows = getRow();
-    int cols = getColumn();
-    for (int i = 0; i < rows; ++i) {
-        m_cells[i].resize(cols + 1);
-        for (int j = cols; j > c; --j) {
-            m_cells[i][j] = m_cells[i][j-1];
-        }
-        m_cells[i][c] = column[i];
-    }
+    	int cols = getColumn();
+    	for (int i = 0; i < rows; ++i) {
+        	m_cells[i].resize(cols + 1);
+        	for (int j = cols; j > c; --j) {
+            		m_cells[i][j] = m_cells[i][j-1];
+        	}
+        	m_cells[i][c] = column[i];
+    	}
 }
 
 void Spreadsheet::removeColumn(int c)
 {
 	int rows = m_cells.size();
-    int cols = m_cells[0].size();
-    for (int i = 0; i < rows; ++i) {
-        for (int j = c; j < cols - 1; ++j) {
-            m_cells[i][j] = m_cells[i][j+1];
-        }
-        m_cells[i].resize(cols - 1);
-    }
+    	int cols = m_cells[0].size();
+    	for (int i = 0; i < rows; ++i) {
+        	for (int j = c; j < cols - 1; ++j) {
+            		m_cells[i][j] = m_cells[i][j+1];
+        	}
+        	m_cells[i].resize(cols - 1);
+    	}
 }
 
 void Spreadsheet::swapRows(int r1, int r2)
 {
 	std::vector<Cell> temp = m_cells[r1];
-    m_cells[r1] = m_cells[r2];
-    m_cells[r2] = temp;
+    	m_cells[r1] = m_cells[r2];
+    	m_cells[r2] = temp;
 }
 
 void Spreadsheet::swapColumns(int c1, int c2)
 {
 	int rows = m_cells.size();
-    for (int i = 0; i < rows; ++i) {
-        Cell temp = m_cells[i][c1];
-        m_cells[i][c1] = m_cells[i][c2];
-        m_cells[i][c2] = temp;
-    }
+    	for (int i = 0; i < rows; ++i) {
+        	Cell temp = m_cells[i][c1];
+        	m_cells[i][c1] = m_cells[i][c2];
+        	m_cells[i][c2] = temp;
+    	}
 }
